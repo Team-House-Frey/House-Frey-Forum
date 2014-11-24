ede0d6b82c038a7faf623567c7f0cffcec994d13
tr00per92 <zvezdo@mail.bg>
2014-11-24 16:48:29 +0200

    changet input to button in the register page; some minor reformating in the register script

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,55 +1,56 @@
-(function () {
-    var PARSE_APP_ID = "q8K93DShEidGUj4LnNjUtdc0ifunrQLgC6J1F6h3";
-    var PARSE_REST_API_KEY = "VAkyH0zeF83ZB5BHHdRs7iLXFtmOBRZqj2J5kQBF";
-    var headers = {
-        "X-Parse-Application-Id": PARSE_APP_ID,
-        "X-Parse-REST-API-Key": PARSE_REST_API_KEY
+(function() {
+    var HEADERS = {
+        "X-Parse-Application-Id": 'q8K93DShEidGUj4LnNjUtdc0ifunrQLgC6J1F6h3',
+        "X-Parse-REST-API-Key": 'VAkyH0zeF83ZB5BHHdRs7iLXFtmOBRZqj2J5kQBF'
     };
 
-    $(function () {
-        $("#registrationForm").validate();
+    $.ajaxSetup({
+        headers: HEADERS,
+        error: ajaxError
+    });
 
-        $('input').keyup(function() {
-            var $th = $(this);
-            $th.val( $th.val()
-                .replace(/[^a-zA-Z0-9_@\.]/g, function(str) {
-                    alert('You typed " ' + str + ' ".\n\nPlease use only letters, numbers and _.');
+    $(function() {
+        $('#registrationForm').validate();
+        $('input').keyup(validateInput);
+        $('#submit').click(registerUser);
+    });
+
+    function validateInput() {
+        var $this = $(this);
+        $this.val($this.val()
+            .replace(/[^a-zA-Z0-9_@\.]/g, function(str) {
+                alert('You typed " ' + str + ' ".\n\nPlease use only letters, numbers and _.');
                 return '';
             }));
+    }
+
+    function registerUser() {
+        var username = $('#username').val();
+        var password = $('#password').val();
+        var firstName = $('#firstName').val();
+        var lastName = $('#lastName').val();
+        var email = $('#email').val();
+
+        $.ajax({
+            method: 'POST',
+            url: 'https://api.parse.com/1/classes/_User',
+            data: JSON.stringify({
+                'username' : username,
+                'password' : password,
+                'firstName' : firstName,
+                'lastName' : lastName,
+                'email' : email
+            }),
+            contentType: 'application/json',
+            success: successfullyRegisterUser
         });
+    }
 
-        $('#submit').click(registerUser);
+    function successfullyRegisterUser() {
+        alert('User successfully registered.');
+    }
 
-        function registerUser(){
-            var username = $('#username').val();
-            var password = $('#password').val();
-            var firstName = $('#firstName').val();
-            var lastName = $('#lastName').val();
-            var email = $('#email').val();
-
-            $.ajax({
-                method: 'POST',
-                headers: headers,
-                url: 'https://api.parse.com/1/classes/_User',
-                data: JSON.stringify({
-                    "username" : username,
-                    "password" : password,
-                    "firstName" : firstName,
-                    "lastName" : lastName,
-                    "email" : email
-                }),
-                contentType: 'application/json',
-                success: successfullyRegisterUser,
-                error: ajaxError
-            });
-        }
-
-        function successfullyRegisterUser(){
-            alert('User successfully registered.')
-        }
-
-        function ajaxError(){
-            alert('Error')
-        }
-    })
+    function ajaxError() {
+        alert('Ajax Error');
+    }
 })();
