27a32cd2b4aef536bda273ce60c2caed13384a11
Petko-Petkov <Petko.Hr.Petkov@gmail.com>
2014-11-25 16:44:29 +0200

    Login form update. Working cookies.

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     var HEADERS = {
         'X-Parse-Application-Id': 'q8K93DShEidGUj4LnNjUtdc0ifunrQLgC6J1F6h3',
         'X-Parse-REST-API-Key': 'VAkyH0zeF83ZB5BHHdRs7iLXFtmOBRZqj2J5kQBF'
@@ -10,7 +10,15 @@
     });
 
     $(function () {
+        if ($.cookie('session')) {
+            $('#login-container').hide();
+            $('#logged-in-container').show();
+            $('#welcome-user')
+                .text('Welcome ' + $.cookie('username'));
+        }
+
         $('#submit-login').click(loginUser);
+        $('#logout-btn').click(logoutUser);
     });
 
     function loginUser() {
@@ -26,13 +34,20 @@
     }
 
     function welcomeUser(data) {
-        HEADERS['X-Parse-Session-Token'] = data.sessionToken;
+        // HEADERS['X-Parse-Session-Token'] = data.sessionToken; // not sure if it's needed
+        $.cookie('session', data.sessionToken, {expires: 1});
+        $.cookie('username', data.username, {expires: 1});
 
-        $('#login-form').hide();
-        $('#registration').hide();
-        $('#login-container')
-            .append($('<span>')
-                .text('Welcome ' + data.username));
+        $('#login-container').hide();
+        $('#logged-in-container').show();
+        $('#welcome-user')
+            .text('Welcome ' + $.cookie('username'));
+    }
+
+    function logoutUser() {
+        $.removeCookie('session');
+        $('#welcome-user').html('<strong>Logging out ... </strong>');
+        window.setTimeout('location.reload()', 2000);
     }
 
     function invalidLogin() {
